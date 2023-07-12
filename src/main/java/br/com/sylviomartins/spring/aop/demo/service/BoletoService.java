30d07d57f89aa6e6594cbb819a4b52da7347e611
Sylvio Martins <sylviohmartins@gmail.com>
2023-07-12 19:16:53 -0300

    feat: atualizacao do retry

--- src/main/java/br/com/sylviomartins/spring/aop/demo/service/BoletoService.java
+++ src/main/java/br/com/sylviomartins/spring/aop/demo/service/BoletoService.java
@@ -1,39 +1,27 @@
 package br.com.sylviomartins.spring.aop.demo.service;
 
+import br.com.sylviomartins.spring.aop.demo.annotation.CustomCounter;
+import br.com.sylviomartins.spring.aop.demo.annotation.nested.*;
 import br.com.sylviomartins.spring.aop.demo.domain.document.Boleto;
 import br.com.sylviomartins.spring.aop.demo.domain.enumeration.StatusEnum;
 import br.com.sylviomartins.spring.aop.demo.domain.mapper.BoletoMapper;
 import br.com.sylviomartins.spring.aop.demo.exception.AlreadyExistsException;
 import br.com.sylviomartins.spring.aop.demo.exception.RecordNotFoundException;
 import br.com.sylviomartins.spring.aop.demo.repository.BoletoRepository;
+import br.com.sylviomartins.spring.aop.demo.util.CustomMethodUtils;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import static br.com.sylviomartins.spring.aop.demo.util.CustomMethodUtils.GET_STATUS_TYPE;
 
 @Service
 @RequiredArgsConstructor
-public class BoletoService {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(BoletoService.class);
+public class BoletoService extends BaseService {
 
     private final BoletoRepository boletoRepository;
-
     private final BoletoMapper boletoMapper;
 
-    @Value("${retry.maxAttempts:3}")
-    private int maxAttempts;
-
-    @Value("${retry.delay:1000}")
-    private long delay;
-
-    @Value("#{'${retry.exceptions}'.split(',')}")
-    private List<Class<? extends Exception>> retryExceptions;
-
-    /*@CustomCounter( //
+    @CustomCounter( //
             name = "inclusao", //
             description = "Custom Counter Example", //
             tags = @Tag(name = "serviceName", value = "inclusao"), //
@@ -56,14 +44,14 @@ public class BoletoService {
                             ) //
                     } //
             ) //
-    )*/
+    )
     public Boleto include(final Boleto boleto) throws AlreadyExistsException {
         boleto.setStatus(StatusEnum.INCLUIDO);
 
         try {
             return performWithRetry(() -> {
-                boletoRepository.save(boleto);
-                return boleto;
+                Boleto savedBoleto = boletoRepository.save(boleto);
+                return savedBoleto;
             });
 
         } catch (final Exception unknownException) {
@@ -78,42 +66,12 @@ public class BoletoService {
             final Boleto persisted = boletoRepository.findById(boleto.getId()).orElseThrow(RecordNotFoundException::new);
             return performWithRetry(() -> {
                 boletoMapper.merge(persisted, boleto);
-                return boletoRepository.save(persisted);
+                Boleto savedBoleto = boletoRepository.save(persisted);
+                return savedBoleto;
             });
 
         } catch (final Exception unknownException) {
             throw new RecordNotFoundException();
         }
-    }
-
-    private <T> T performWithRetry(RetryableAction<T> action) throws Exception {
-        int retryCount = 0;
-
-        while (retryCount <= maxAttempts) {
-            try {
-                return action.execute();
-            } catch (Exception ex) {
-                if (retryCount < maxAttempts && isRetryException(ex)) {
-                    logRetryAttempt(retryCount, ex.getMessage(), maxAttempts);
-                    Thread.sleep(delay);
-                }
-                retryCount++;
-            }
-        }
-
-        throw new Exception("Falha após várias tentativas");
-    }
-
-    private boolean isRetryException(Exception ex) {
-        return retryExceptions.stream().anyMatch(e -> e.isInstance(ex));
-    }
-
-    private void logRetryAttempt(int retryCount, String reason, int maxAttempts) {
-        LOGGER.warn("[Retry: {}/{} - Reason: '{}']", retryCount, maxAttempts, reason);
-    }
-
-    @FunctionalInterface
-    private interface RetryableAction<T> {
-        T execute() throws Exception;
     }
 }
